530c4488d2d18ca7909f74ef71b37c21db8e510f
thanhvanvt123 <63654547+thanhvanvt123@users.noreply.github.com>
2021-04-22 09:30:10 +0700

    Update intructionSearchHisOfStudent.sql

--- intructionSearchHisOfStudent.sql
+++ intructionSearchHisOfStudent.sql
@@ -11,4 +11,3 @@ Select q.*, u.username from QuizHistory q, Users u where q.studentId = u.id and 
 ---search history by subject and status 'faild'
 Select q.*, u.username from QuizHistory q, Users u where q.studentId = u.id and q.studentId = 8 and q.subjectid = 1 and q.correctAnswer < (q.numOfQuiz/2)
 
--- dạ của chị hết 500k ạ
